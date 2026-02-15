474c4d3dba9dae9ebbd9f80b3a5a5c28c21a1a9a
medu1122 <huynhthinh61@gmail.com>
2026-02-15 13:21:50 +0700

    tao truoc plan

---
+++ src/main/java/me/skibidi/rOLEmmo/ROLEmmo.java
@@ -0,0 +1,17 @@
+package me.skibidi.rOLEmmo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ROLEmmo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
